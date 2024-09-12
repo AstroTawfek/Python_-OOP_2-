417e92c2051aa174e81f31c79df0de99fafddfcc
Tawfek <tawfekjerry@gmail.com>
2024-09-13 01:21:46 +0600

    Adding lab task 2

---
+++ Lab_task_02/Loop_Practice.py
@@ -0,0 +1,12 @@
+for x in range(1,11):
+    print(x,end=" ")      # Output : 1 2 3 4 5 6 7 8 9 10
+print()
+
+for y in range(0,20,2):
+    print(y,end=" ")    # Output : 0 2 4 6 8 10 12 14 16 18
+print()
+
+a=10
+for x in range(a):
+    print(x,end=" ")   # Output : 0 1 2 3 4 5 6 7 8 9
+print()
