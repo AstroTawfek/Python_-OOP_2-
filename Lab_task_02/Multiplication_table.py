316386f18c5fe24ca47296129ddf8469aafaaa10
Tawfek <tawfekjerry@gmail.com>
2024-09-19 02:57:52 +0600

    adding comment for the multiplication_table at lab task 2

--- Lab_task_02/Multiplication_table.py
+++ Lab_task_02/Multiplication_table.py
@@ -1,11 +1,17 @@
+# Defining a function called mult_table that takes one argument x
 def mult_table(x):
-    for i in range(1,11,1):
-        if i==4:
+    # Loop through numbers from 1 to 10 (excluding 11) with a step of 1
+    for i in range(1, 11, 1):
+        # If the current number is 4, skip this iteration and move to the next one
+        if i == 4:
             continue
-
+        
+        # Printing the multiplication table for the number x and the current number i
         print(f"{x} * {i} = {x*i}")
         
-        if i==8:
+        # If the current number is 8, break out of the loop
+        if i == 8:
             break
     
+# Asking the user to enter a number and pass it to the mult_table function
 mult_table(int(input("Enter a Number : ")))
