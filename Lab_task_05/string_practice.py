2606fb9b5c171c7492e232e51dfd7c8d74bfc8cf
Tawfek <tawfekjerry@gmail.com>
2024-09-25 20:44:42 +0600

    Adding proper commenting to better of code in string_practice.py

--- Lab_task_05/string_practice.py
+++ Lab_task_05/string_practice.py
@@ -1,28 +1,64 @@
+# Assigning values to variables
 a = "hello"
 b = "b2b2b2"
 c = "3g3g       "
-d = a+" "+b+" "+c
+
+# Concatenating strings
+d = a + " " + b + " " + c
 print(d)
 
-print("Length : ",len(d))
+# Printing the length of the string
+print("Length : ", len(d))
+
+# Printing the string without the last 3 characters
 print(d[:-3])
+
+# Printing the string in reverse order
 print(d[::-1])
 
-print("Upper : ",d.upper())
-print("Lower : ",d.lower())
-print("Strip : ",d.strip())
-print("length after strip : ",len(d.strip()))
-print("Is there any digit : ", d.isdigit())
-print("Find 39 : ", d.find("3g"))
-print("capitalize : ", d.capitalize())
-print("IsAlNum : ", d.isalnum())
-print("counting b2 : ", d.count("a2"))
-print("using split fun. : ", d.split())
-print("Counting a2 : ",d.count("a2"))
-print("Replace  : " , d.replace("hello" , "python"))
-print("Swapcase : ", d.swapcase())
-print("Using title fun. : ",d.title())
+# Printing the string in uppercase
+print("Upper : ", d.upper())
 
+# Printing the string in lowercase
+print("Lower : ", d.lower())
+
+# Printing the string with leading and trailing whitespaces removed
+print("Strip : ", d.strip())
+
+# Printing the length of the string after removing leading and trailing whitespaces
+print("length after strip : ", len(d.strip()))
+
+# Checking if the string contains only digits
+print("Is there any digit : ", d.isdigit())
+
+# Finding the index of the first occurrence of "3g"
+print("Find 39 : ", d.find("3g"))
+
+# Capitalizing the first letter of the string
+print("capitalize : ", d.capitalize())
+
+# Checking if the string contains only alphanumeric characters
+print("IsAlNum : ", d.isalnum())
+
+# Counting the occurrences of "a2" in the string
+print("counting b2 : ", d.count("a2"))
+
+# Splitting the string into a list of words
+print("using split fun. : ", d.split())
+
+# Counting the occurrences of "a2" in the string
+print("Counting a2 : ", d.count("a2"))
+
+# Replacing "hello" with "python" in the string
+print("Replace  : " , d.replace("hello" , "python"))
+
+# Swapping the case of the string
+print("Swapcase : ", d.swapcase())
+
+# Capitalizing the first letter of each word in the string
+print("Using title fun. : ", d.title())
+
+# Checking if "a2" is present in the string
 if "a2" in d :
     print("true")
 else:
