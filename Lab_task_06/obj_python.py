62693647a3cb5b276cd353b8cc09921495c50171
Tawfek <tawfekjerry@gmail.com>
2024-12-04 20:14:43 +0600

    uploading  Lab Task 6

---
+++ Lab_task_06/obj_python.py
@@ -0,0 +1,15 @@
+class Student :
+    def __init__(self,a,id):
+        self.name = a
+        self.id = id
+    def display(self):
+        print("Name : ",self.name)
+        print("Id : ", self.id)
+
+s1 = Student("Tawfek", 1827)
+s1.display()
+print(s1.name)
+
+s2 = Student("negar" , 1813)
+print(s2.name , s2.id)
+s2.display()
